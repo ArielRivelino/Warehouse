bc0a70346729f0015cbeb1894ed471decb40d904
Willy Faqurokhim <willyfaqurokhim@gmail.com>
2020-06-14 00:24:53 +0700

    Update warehouse.sql

--- warehouse.sql
+++ warehouse.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
--- Host:                         localhost
--- Server version:               5.6.26 - MySQL Community Server (GPL)
--- Server OS:                    Win32
--- HeidiSQL Version:             9.4.0.5125
+-- Host:                         127.0.0.1
+-- Server version:               10.4.11-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             11.0.0.5919
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -10,6 +10,12 @@
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for warehouse
+DROP DATABASE IF EXISTS `warehouse`;
+CREATE DATABASE IF NOT EXISTS `warehouse` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `warehouse`;
 
 -- Dumping structure for table warehouse.master_barang
 DROP TABLE IF EXISTS `master_barang`;
@@ -29,12 +35,16 @@ CREATE TABLE IF NOT EXISTS `master_barang` (
   KEY `id_satuan` (`id_satuan`),
   CONSTRAINT `FK_master_barang_master_jenis` FOREIGN KEY (`id_jenis`) REFERENCES `master_jenis` (`id_jenis`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `FK_master_barang_master_satuan` FOREIGN KEY (`id_satuan`) REFERENCES `master_satuan` (`id_satuan`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
--- Dumping data for table warehouse.master_barang: ~0 rows (approximately)
+-- Dumping data for table warehouse.master_barang: ~4 rows (approximately)
 /*!40000 ALTER TABLE `master_barang` DISABLE KEYS */;
 INSERT INTO `master_barang` (`id_barang`, `id_jenis`, `id_satuan`, `item_name`, `stock`, `blok`, `code`, `line`, `column`, `status`) VALUES
-	(1, 1, 2, 'Kertas', 100, 'c', 'c', 'c', 'c', 0);
+	(1, 1, 2, 'Kertas', 100, 'c', 'c', 'c', 'c', 1),
+	(2, 1, 1, 'PC Desktop Asus', 20, 'A', 'A', 'A1', 'A1', 1),
+	(3, 2, 1, 'EXCAVA 200', 100, 'D', 'D', 'D', 'D', 1),
+	(4, 2, 1, 'TELEHANDLER', 200, 'D', 'D', 'D2', 'D2', 1),
+	(5, 3, 2, 'Sapu', 100, 'S', 'S', 'S', 'S', 1);
 /*!40000 ALTER TABLE `master_barang` ENABLE KEYS */;
 
 -- Dumping structure for table warehouse.master_jenis
@@ -71,13 +81,37 @@ INSERT INTO `master_satuan` (`id_satuan`, `satuan`) VALUES
 -- Dumping structure for table warehouse.t_access
 DROP TABLE IF EXISTS `t_access`;
 CREATE TABLE IF NOT EXISTS `t_access` (
+  `id_access` int(11) NOT NULL AUTO_INCREMENT,
   `role_id` int(11) NOT NULL,
   `menu_id` int(11) NOT NULL,
-  `aksi` varchar(258) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `aksi` varchar(258) NOT NULL,
+  PRIMARY KEY (`id_access`),
+  KEY `role_id` (`role_id`),
+  KEY `menu_id` (`menu_id`),
+  CONSTRAINT `FK_t_access_t_menu` FOREIGN KEY (`menu_id`) REFERENCES `t_menu` (`menu_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_t_access_user_role` FOREIGN KEY (`role_id`) REFERENCES `user_role` (`role_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
 
--- Dumping data for table warehouse.t_access: ~0 rows (approximately)
+-- Dumping data for table warehouse.t_access: ~2 rows (approximately)
 /*!40000 ALTER TABLE `t_access` DISABLE KEYS */;
+INSERT INTO `t_access` (`id_access`, `role_id`, `menu_id`, `aksi`) VALUES
+	(4, 2, 5, '1,2,3'),
+	(7, 2, 20, '1,2,3'),
+	(8, 2, 21, '1,2,3'),
+	(9, 3, 20, '1,3'),
+	(10, 3, 21, '1,3'),
+	(12, 4, 5, '1,2,3'),
+	(13, 4, 6, '1,2,3'),
+	(14, 4, 12, '1,2,3'),
+	(15, 4, 13, '1,2,3'),
+	(16, 4, 14, '1,2,3'),
+	(17, 4, 15, '1,2,3'),
+	(18, 4, 16, '1,2,3'),
+	(19, 4, 17, '1,2,3'),
+	(20, 4, 18, '1,2,3'),
+	(21, 4, 19, '1,2,3'),
+	(22, 4, 20, '1,2,3'),
+	(23, 4, 21, '1,2,3');
 /*!40000 ALTER TABLE `t_access` ENABLE KEYS */;
 
 -- Dumping structure for table warehouse.t_menu
@@ -89,9 +123,9 @@ CREATE TABLE IF NOT EXISTS `t_menu` (
   `status` int(1) NOT NULL,
   `type` int(11) NOT NULL,
   PRIMARY KEY (`menu_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
 
--- Dumping data for table warehouse.t_menu: ~0 rows (approximately)
+-- Dumping data for table warehouse.t_menu: ~7 rows (approximately)
 /*!40000 ALTER TABLE `t_menu` DISABLE KEYS */;
 INSERT INTO `t_menu` (`menu_id`, `menu_name`, `url`, `status`, `type`) VALUES
 	(1, 'Master', 'fa-cog', 1, 0),
@@ -100,7 +134,16 @@ INSERT INTO `t_menu` (`menu_id`, `menu_name`, `url`, `status`, `type`) VALUES
 	(4, 'Setting', 'fa-wrench', 1, 0),
 	(5, 'Master Barang', 'barang', 1, 1),
 	(6, 'Master User', 'user', 1, 1),
-	(7, 'Permintaan Stok', 'permintaan_stok', 1, 2);
+	(12, 'Barang', 'Barang', 1, 3),
+	(13, 'Permintaan Stok', 'Permintaan', 1, 3),
+	(14, 'Permintaan Barang Baru', 'Permintaan', 1, 3),
+	(15, 'Jenis', 'Jenis', 1, 4),
+	(16, 'Satuan', 'Satuan', 1, 4),
+	(17, 'Menu', 'Menu', 1, 4),
+	(18, 'User Role', 'User_role', 1, 4),
+	(19, 'User Access', 'User_access', 1, 4),
+	(20, 'Permintaan Stok', 'Permintaan/stok', 1, 2),
+	(21, 'Permintaan Barang Baru', 'Permintaan/baru', 1, 2);
 /*!40000 ALTER TABLE `t_menu` ENABLE KEYS */;
 
 -- Dumping structure for table warehouse.t_request
@@ -116,11 +159,18 @@ CREATE TABLE IF NOT EXISTS `t_request` (
   `approval_date` datetime NOT NULL,
   `status` int(1) NOT NULL,
   `proof` varchar(258) NOT NULL,
-  PRIMARY KEY (`request_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`request_id`),
+  KEY `id_barang` (`id_barang`),
+  KEY `nik` (`nik`),
+  CONSTRAINT `FK_t_request_master_barang` FOREIGN KEY (`id_barang`) REFERENCES `master_barang` (`id_barang`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_t_request_user` FOREIGN KEY (`nik`) REFERENCES `user` (`nik`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
--- Dumping data for table warehouse.t_request: ~0 rows (approximately)
+-- Dumping data for table warehouse.t_request: ~1 rows (approximately)
 /*!40000 ALTER TABLE `t_request` DISABLE KEYS */;
+INSERT INTO `t_request` (`request_id`, `nik`, `request_type`, `id_barang`, `amount`, `information`, `request_date`, `approval_date`, `status`, `proof`) VALUES
+	(1, '1234', 0, 3, 200, 'Ini Informasi untuk permintaan stok', '2020-06-13 15:13:23', '0000-00-00 00:00:00', 1, 'ini proof'),
+	(2, '1234', 1, 5, 100, 'ini informasi untuk sapu baru', '2020-06-13 16:14:20', '0000-00-00 00:00:00', 1, 'ini proof untuk sapu');
 /*!40000 ALTER TABLE `t_request` ENABLE KEYS */;
 
 -- Dumping structure for table warehouse.user
@@ -132,23 +182,33 @@ CREATE TABLE IF NOT EXISTS `user` (
   `staff` varchar(258) NOT NULL,
   `role_id` int(11) NOT NULL,
   `email` varchar(258) NOT NULL,
-  PRIMARY KEY (`nik`)
+  PRIMARY KEY (`nik`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `FK_user_user_role` FOREIGN KEY (`role_id`) REFERENCES `user_role` (`role_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table warehouse.user: ~0 rows (approximately)
+-- Dumping data for table warehouse.user: ~2 rows (approximately)
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`nik`, `name`, `password`, `staff`, `role_id`, `email`) VALUES
+	('1234', 'Saya Gudang', '1234', 'Gudang', 3, 'satuduatigaempat@gmail.com'),
+	('4321', 'Saya Kepala', '4321', 'Kepala', 3, 'sayakepala@gmail.com'),
+	('admin', 'Admin Keren', 'admin', 'Administrator', 4, 'admin@warehouse.com');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 -- Dumping structure for table warehouse.user_role
 DROP TABLE IF EXISTS `user_role`;
 CREATE TABLE IF NOT EXISTS `user_role` (
-  `role_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL AUTO_INCREMENT,
   `role` varchar(258) NOT NULL,
   PRIMARY KEY (`role_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
--- Dumping data for table warehouse.user_role: ~0 rows (approximately)
+-- Dumping data for table warehouse.user_role: ~2 rows (approximately)
 /*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
+INSERT INTO `user_role` (`role_id`, `role`) VALUES
+	(2, 'Staff Gudang'),
+	(3, 'Kepala Cabang'),
+	(4, 'Admin Web');
 /*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
